ea850b04469a57d171c233eeec2bb5880af80e15
ptokmakov <ptokmakov501@gmail.com>
2016-06-18 14:25:58 +0700

    Update qe.h

--- src/qe.h
+++ src/qe.h
@@ -3,6 +3,7 @@
 #define FOUND_2_ROOTS 2
 #define FOUND_1_ROOT 1
 #define ROOT_NOT_FOUND 0
+#define NON_QUADRATIC_EQUATION 0
 
 int quadratic_equation(float a, float b, float d, float *x1, float *x2);
 
