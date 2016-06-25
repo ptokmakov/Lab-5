0f5ab76d6a9136891b5c2e1a2286aa85c4cad2af
Токмаков Павел Андреевич <ri58s18@awp10.csc.local>
2016-06-25 10:58:20 +0600

    Revert "Update qe.h"
    This reverts commit ea850b04469a57d171c233eeec2bb5880af80e15.

--- src/qe.h
+++ src/qe.h
@@ -3,7 +3,6 @@
 #define FOUND_2_ROOTS 2
 #define FOUND_1_ROOT 1
 #define ROOT_NOT_FOUND 0
-#define NON_QUADRATIC_EQUATION 0
 
 int quadratic_equation(float a, float b, float d, float *x1, float *x2);
 
