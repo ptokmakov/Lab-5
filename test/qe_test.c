6f6c05152e8788b4c4fa559deddab6667e81539a
Токмаков Павел Андреевич <ri58s18@awp10.csc.local>
2016-06-25 10:59:02 +0600

    Revert "Update qe_test.c"
    This reverts commit 4c8fc970479459993d05394da08b765d869a041b.

--- test/qe_test.c
+++ test/qe_test.c
@@ -50,16 +50,3 @@ CTEST(solution_of_quadratic_equations, no_root) {
 	// Then
 	ASSERT_EQUAL(ROOT_NOT_FOUND, result);
 }
-
-CTEST(solution_of_quadratic_equations, non_quadratic_equation) {
-	// Given
-	const float a = 0;
-	const float b = 25;
-	const float d = 125;
-
-	// When
-	float x1, x2;
-	const int result = quadratic_equation(a, b, d, &x1, &x2);
-
-	// Then
-	ASSERT_EQUAL(ROOT_NOT_FOUND, NON_QUADRATIC_EQUATION);
